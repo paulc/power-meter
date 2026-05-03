0c525acc3e894f460b98df4a6134827064627c79
paulc <212168+paulc@users.noreply.github.com>
2026-05-03 23:43:55 +0100

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+pub mod ble_task;
+pub mod c6_lcd;
+pub mod encoder;
+pub mod ina219;
