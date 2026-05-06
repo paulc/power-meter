1a49fd47ea83281123020aca6e446ad36cd9be4d
paulc <212168+paulc@users.noreply.github.com>
2026-05-06 23:37:07 +0100

    Use poll for SSE stream

--- utils/sse-stream.sh
+++ utils/sse-stream.sh
@@ -4,6 +4,12 @@ BLESCAN=~/.local/bin/blescan
 STDIN_SSE=~/.local/bin/stdin-sse
 JQ=jq
 
-$BLESCAN poll --name INA219 --characteristic 00000003-9b04-4347-98ff-57e8f7803509 --decode 00000003-9b04-4347-98ff-57e8f7803509::u64 --interval 1 --json | \
-    $JQ --unbuffered --compact-output 'paths(scalars) as $p | select(getpath($p) and ($p[-1]=="value")) | {value: getpath($p)}' | \
+# Set notify interval to 1s
+$BLESCAN write --name INA219 --characteristic 00000005-9b04-4347-98ff-57e8f7803509::1_u32
+$BLESCAN notify --name INA219 --characteristic 00000003-9b04-4347-98ff-57e8f7803509 --decode 00000003-9b04-4347-98ff-57e8f7803509::u64 --json | \
+    $JQ --unbuffered --compact-output 'select(.notification) | {value: .notification.value}' | \
     $STDIN_SSE --endpoint /update --event-type update --cors --debug --index "$(dirname $0)/ina219_reading.html"
+
+# $BLESCAN poll --name INA219 --characteristic 00000003-9b04-4347-98ff-57e8f7803509 --decode 00000003-9b04-4347-98ff-57e8f7803509::u64 --interval 1 --json | \
+#     $JQ --unbuffered --compact-output 'paths(scalars) as $p | select(getpath($p) and ($p[-1]=="value")) | {value: getpath($p)}' | \
+#     $STDIN_SSE --endpoint /update --event-type update --cors --debug --index "$(dirname $0)/ina219_reading.html"
